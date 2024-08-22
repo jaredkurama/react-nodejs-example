49ad3ff6a40c322bf484c5814ceab9abd98ef9cc
jaredkurama <137181595+jaredkurama@users.noreply.github.com>
2024-08-22 16:42:53 +0100

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,12 @@
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t jaredkurama/demo-app:my-app:2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push jaredkurama/demo-app:my-app:2.0'
+    }
+} 
+def deployApp() {
+    echo 'deploying the application...'
+} 
+return this
